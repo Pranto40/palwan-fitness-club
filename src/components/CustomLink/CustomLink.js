55b619d7420af49875fbc3d6d35bc36288eefacd
pranto <polasahammedpranto@gmail.com>
2022-04-16 21:22:00 +0600

    header section added

---
+++ src/components/CustomLink/CustomLink.js
@@ -0,0 +1,19 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+function CustomLink ({ children, to, ...props }) {
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+  
+    return (
+      <div>
+        <Link
+          style={{ color: match ? 'white' : 'black', background: match ? 'red' : ''}}
+          to={to}
+          {...props}
+        >
+          {children}
+        </Link>
+      </div>
+    );
+  }
+  export default CustomLink;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import logo from '../../images/logo.png'
+import CustomLink from '../CustomLink/CustomLink';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+              <img className='logo' src={logo} alt="" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <div className="nav-item">
+                <CustomLink to='/'>Home</CustomLink>
+                <CustomLink to='/services'>Services</CustomLink>
+                <CustomLink to='/blog'>Blog</CustomLink>
+                <CustomLink to='/login'>login</CustomLink>
+                <CustomLink to='/signUP'>Sign Up</CustomLink>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+};
+
+export default Header;
