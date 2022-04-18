d832a11d4cce76756efd3a421f56e44b876bcc45
pranto <polasahammedpranto@gmail.com>
2022-04-18 17:13:19 +0600

    services section img and content change

--- src/components/ServicesDetails/ServicesDetails.js
+++ src/components/ServicesDetails/ServicesDetails.js
@@ -2,16 +2,17 @@ import React from 'react';
 import {Card, CardImg } from 'react-bootstrap';
 
 const ServicesDetails = ({servicesItem}) => {
-    const {img, title, description} = servicesItem
+    const {img, title, description, price} = servicesItem
     return (
         <div className='servicesInfo'>
             <CardImg className='top' src={img} />
-                <Card.Body>
-                    <Card.Title>{title}</Card.Title>
+                <Card.Body className='border rounded border-ligh border-top-0'>
+                    <Card.Title><h3>{title}</h3></Card.Title>
+                    <Card.Title>Price: {price}</Card.Title>
                     <Card.Text>
                     {description}
                     </Card.Text>
-                    <button className="btn btn-main">Go somewhere</button>
+                    <button className="btn btn-main">Buy Now</button>
                 </Card.Body>
         </div>        
     );
